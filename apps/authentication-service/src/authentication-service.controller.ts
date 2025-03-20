6deab5a0224b584d0fd39d737d59df6d6f4d1147
Ajay <cloud@Ajays-MacBook-Pro.local>
2025-03-20 23:24:05 +0530

    added: prisma client

--- apps/authentication-service/src/authentication-service.controller.ts
+++ apps/authentication-service/src/authentication-service.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthenticationServiceService } from './authentication-service.service';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AuthenticationServiceController {
-  constructor(private readonly authenticationServiceService: AuthenticationServiceService) {}
+  constructor(
+    private readonly authenticationServiceService: AuthenticationServiceService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.authenticationServiceService.getHello();
+  async getHello(): Promise<any> {
+    return this.prisma.post.count();
   }
 }
--- apps/authentication-service/src/authentication-service.module.ts
+++ apps/authentication-service/src/authentication-service.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationServiceController } from './authentication-service.controller';
 import { AuthenticationServiceService } from './authentication-service.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AuthenticationServiceController],
-  providers: [AuthenticationServiceService],
+  providers: [AuthenticationServiceService, PrismaService],
 })
 export class AuthenticationServiceModule {}
